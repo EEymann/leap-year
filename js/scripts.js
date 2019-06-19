2d2afd742d7e4ad3162830d1181c496c7e613508
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2019-06-19 11:46:21 -0700

    any year divisible by 4 returns true.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 //business logic
 
 var leapYear = function(year) {
-  return false;
+  if (year % 4 === 0) { 
+    return true; // any year divisible by 4 should return true. Otherwise, it will return false.
+  } else {
+    return false;
+  }
 };
 
 
