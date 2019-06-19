1c6711ea8ca36b073206bbd6079d19ff86d0e091
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2019-06-19 12:06:43 -0700

    refactor business logic so that any year divisible by 4 and is not divisible by 100 returns false.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
 //business logic
 
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false; // any year divisible by 100 should return false. 
-  } else if (year % 4 === 0) { 
-    return true; // any year divisible by 4 should return true. 
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
   } else {
     return false;
   }
 };
-
 
 //user interface logic that will retrieve user input from our form and call our business logic leapYear()
 
